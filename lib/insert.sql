4987b08a30daba429b61e020e420c0bca7b92d73
Dante4K18 <dan@dan>
2024-10-20 15:40:37 +0300

    completed-bears-lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'brown', 'playful', 1),
+('Rowdy', 3, 'M', 'black', 'aggressive', 1),
+('Tabitha', 4, 'F', 'cinnamon', 'gentle', 1),
+('Sergeant Brown', 6, 'M', 'dark brown', 'protective', 1),
+('Melissa', 2, 'F', 'golden', 'curious', 1),
+('Grinch', 7, 'M', 'green', 'grumpy', 0),
+('Wendy', 1, 'F', 'white', 'friendly', 1),
+(NULL, 2, 'M', 'grey', 'shy', 1);  -- Set name to NULL
